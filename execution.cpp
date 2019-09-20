5692aae15a0bbf8b29fd0de2ccde0da13d511b4a
Harsh Singh <53997601+Analystharsh@users.noreply.github.com>
2019-09-20 01:54:10 -0700

    Update execution.cpp

--- execution.cpp
+++ execution.cpp
@@ -4,7 +4,7 @@
  using namespace std;
  
  int main(){
-   string my ="On this machine, the steady_clock and high_resolution_clock have the same characteristics, both are non-adjustable clocks with nanosecond precision. On the other hand, the system_clock is adjustable and it has only microsecond precision.For comparison, this is the output of the program, compiled with Visual Studio 2012, on a Windows 7 machine:";
+      string my ="On this machine, the steady_clock and high_resolution_clock have the same characteristics, both are non-adjustable clocks with nanosecond precision. On the other hand, the system_clock is adjustable and it has only microsecond precision.For comparison, this is the output of the program, compiled with Visual Studio 2012, on a Windows 7 machine:";
       auto start1 = chrono::steady_clock::now();
       int a =my.find("Windows",0);
       auto end1 = chrono::steady_clock::now();
@@ -15,5 +15,5 @@
       auto end2 = chrono::steady_clock::now();
       auto diff2 = end2 - start2;
       cout << chrono::duration <double, nano> (diff2).count() << " ns" << endl;
-     return 0;
+      return 0;
  }
